dad1c512b35eb4a7ea5b00e91a4423d5a63d29f3
Clancy Thomas <clancyvthomas@gmail.com>
2022-05-25 18:17:48 -0600

    Created wordHandler class

---
+++ wordHandler.py
@@ -0,0 +1,15 @@
+from random import randint
+
+class wordHandler:
+    
+    def __init__(self):
+        self.secretWord = ""
+        self.wordList = ["Alphabet", "Couch", "Potato", "Garden", "Television"]
+
+    def createSecretWord(self):
+        x = len(self.wordList)
+        randomIndex = randint(0, x)
+        self.secretWord = self.wordList[randomIndex]
+
+    def getSecretWord(self):
+        return self.secretWord
