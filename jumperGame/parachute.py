a8390fbf49617ab4d00d42cb3dcdcf164619a82c
Calebfinn143 <bar17106@byui.edu>
2022-05-28 13:51:22 -0600

    grass on lose commit

--- jumperGame/parachute.py
+++ jumperGame/parachute.py
@@ -7,7 +7,7 @@ class Image:
     def updateParachute(self, livesLeft):
         match livesLeft:
             case 0:
-                self.image = "\n  X \n -|-\n / \ \n\nYOU LOST!!!\n"
+                self.image = "\n  X \n -|-\n / \ \n\n ^^^^^^^^^^^ \n\n YOU LOST!!!\n"
             case 1:
                 self.image = "\n \  /\n  O\n -|-\n / \ \n\n^^^^^^^^^^^"
             case 2:
